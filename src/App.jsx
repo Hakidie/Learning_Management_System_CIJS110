da7d7b9da74055cc4608177676b6adb700809921
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-03-30 23:13:36 +0700

    no changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
 import './App.css'
-// import Header from './components/Header/Header';
-// import Footer from './components/Footer/Footer';
+import { Routes, Route } from 'react-router-dom';
+
+import UserPage from './pages/User/User';
+import UserCoursesPage from './pages/User_Courses/User_Courses';
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
-      {/* <Footer /> */}
+      <Routes>
+        <Route path="/user" element={<UserPage />}>
+          {/* These components will appear inside the <Outlet /> */}
+          <Route index element={<div>Profile</div>} />
+          <Route path="profile" element={<div>Profile</div>} />
+          <Route path="my_courses" element={<UserCoursesPage />} />
+          <Route path="my_teachers" element={<div>My Teachers</div>} />
+          <Route path="my_messages" element={<div>My Messages</div>} />
+          <Route path="my_reviews" element={<div>My Reviews</div>} />
+        </Route>
+      </Routes>
     </>
   )
 }
