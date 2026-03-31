fc749855ad2f9a3d9404d325d5cf25b570f02a08
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-03-31 21:42:57 +0700

    update cards for page

--- src/pages/User_Teacher/User_Teacher.jsx
+++ src/pages/User_Teacher/User_Teacher.jsx
@@ -2,12 +2,21 @@ import './User_Teacher.css';
 
 import UserPageName from '../../components/User_Page_Name/User_Page_Name';
 import Toolbar from '../../components/Toolbar/Toolbar';
+import TeacherCard from '../../components/Teacher_Card_User/Teacher_Card_User';
+
+import teachersData from '../../sample_data/teachersData';
 
 function UserTeacher() {
   return (
     <div className='user-teacher-container'>
-      <UserPageName pageName={"Teacher"} total={10}/>
+      <UserPageName pageName={"Teacher"} total={10} />
       <Toolbar />
+
+      <div className='teacher-container'>
+        {teachersData.map((item) => (
+          <TeacherCard key={item.id} teacher={item} />
+        ))}
+      </div>
     </div>
   );
 }
