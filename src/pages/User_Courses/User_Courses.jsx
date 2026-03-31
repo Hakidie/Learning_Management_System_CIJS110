65f4425edac9ce30c84e7b88eeb6ab0b052651aa
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-03-31 21:42:41 +0700

    update cards for page

--- src/pages/User_Courses/User_Courses.jsx
+++ src/pages/User_Courses/User_Courses.jsx
@@ -2,12 +2,20 @@ import './User_Courses.css';
 
 import UserPageName from '../../components/User_Page_Name/User_Page_Name';
 import Toolbar from '../../components/Toolbar/Toolbar';
+import CourseCard from '../../components/Course_Card_User/Course_Card_User';
+
+import coursesData from '../../sample_data/coursesData';
 
 function UserCourses() {
   return (
     <div className='user-courses-container'>
-      <UserPageName pageName={"Courses"} totalCourses={10}/>
+      <UserPageName pageName={"Courses"} total={coursesData.length} />
       <Toolbar />
+      <div className='courses-container'>
+        {coursesData.map((item) => (
+          <CourseCard key={item.id} course={item} />
+        ))}
+      </div>
     </div>
   );
 }
