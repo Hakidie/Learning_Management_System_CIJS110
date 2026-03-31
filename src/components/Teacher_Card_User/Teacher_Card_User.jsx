8bd1ad1f45f8de8dbe2f4045fd1a84010b8543cd
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-03-31 21:41:50 +0700

    add teacher card component

---
+++ src/components/Teacher_Card_User/Teacher_Card_User.jsx
@@ -0,0 +1,27 @@
+import './Teacher_Card_User.css';
+
+import { Link } from 'react-router-dom';
+import mailIcon from '../../assets/Icons/mail.svg';
+
+function TeacherCard({ teacher }) {
+  return (
+    <div className='teacher-card-container'>
+      <Link to={`/teacher/message/${teacher.id}`} className='course-card-link' style={{ textDecoration: 'none', color: 'inherit' }}>
+        <div className='teacher-card'>
+          <div class="teacher-picture">
+            <img src={teacher.img} alt={teacher.name} />
+            <h3>{teacher.name}</h3>
+            <p>{teacher.role}</p>
+          </div>
+
+          <button class="teacher-send-message">
+            Send Message
+            <img src={mailIcon} />
+          </button>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default TeacherCard;
