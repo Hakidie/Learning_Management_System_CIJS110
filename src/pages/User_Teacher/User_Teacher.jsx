19950d7f943bf35809b20a8a69ac1966f0d61e65
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-04-01 22:16:53 +0700

    add search, sort, filter and get data function

--- src/pages/User_Teacher/User_Teacher.jsx
+++ src/pages/User_Teacher/User_Teacher.jsx
@@ -1,19 +1,36 @@
 import './User_Teacher.css';
 
+import React, { useState, useEffect } from 'react';
+
 import UserPageName from '../../components/User_Page_Name/User_Page_Name';
 import Toolbar from '../../components/Toolbar/Toolbar';
 import TeacherCard from '../../components/Teacher_Card_User/Teacher_Card_User';
 
-import teachersData from '../../sample_data/teachersData';
+import getResource from '../../hooks/getResources';
+import { processDataUtils } from '../../utils/toolbar';
 
 function UserTeacher() {
+  const teachersData = getResource("teachers");
+  const [currentSort, setCurrentSort] = useState("Newest");
+  const [currentFilter, setCurrentFilter] = useState("Filter");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Sort, filter and search processing 
+  const displayedTeachers = processDataUtils(teachersData, currentFilter, currentSort, searchQuery);
+
   return (
     <div className='user-teacher-container'>
-      <UserPageName pageName={"Teacher"} total={10} />
-      <Toolbar />
+      <UserPageName pageName={"Teacher"} total={displayedTeachers.length} />
+      <Toolbar
+        currentSort={currentSort}
+        currentFilter={currentFilter}
+        onSortChange={setCurrentSort}
+        onFilterChange={setCurrentFilter}
+        onSearch={setSearchQuery}
+      />
 
       <div className='teacher-container'>
-        {teachersData.map((item) => (
+        {displayedTeachers.map((item) => (
           <TeacherCard key={item.id} teacher={item} />
         ))}
       </div>
