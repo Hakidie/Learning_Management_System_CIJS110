f92d8217286b7eb2f91e708189b4caed345688fe
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-04-02 16:36:03 +0700

    move sort and filter button to compoments

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -3,25 +3,13 @@ import './Toolbar.css';
 import React, { useState } from 'react';
 
 import searchIcon from '../../assets/Icons/search.svg';
-import downArrowIcon from '../../assets/Icons/down-arrow.svg';
-import filterIcon from '../../assets/Icons/filter.svg';
+
+import SortButton from '../../components/SortButton/SortButton';
+import FilterButton from '../../components/FilterButton/FilterButton';
 
 function Toolbar({ currentSort, currentFilter, onSortChange, onFilterChange, onSearch }) {
-  // State to track if dropdowns are open
-  const [isSortOpen, setIsSortOpen] = useState(false);
-  const [isFilterOpen, setIsFilterOpen] = useState(false);
+  // Input value of searchbar
   const [inputValue, setInputValue] = useState("");
-
-  // Handlers to update state and close dropdown
-  const handleSortClick = (value) => {
-    setIsSortOpen(false);
-    if (onSortChange) onSortChange(value);
-  };
-
-  const handleFilterClick = (value) => {
-    setIsFilterOpen(false);
-    if (onFilterChange) onFilterChange(value);
-  };
 
   const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
@@ -42,42 +30,17 @@ function Toolbar({ currentSort, currentFilter, onSortChange, onFilterChange, onS
         <img src={searchIcon} alt="search-icon" />
       </div>
 
-      {/* Sort container */}
-      <div className='toolbar-sort-container'>
-        <p>Sort By</p>
+      {/* Sort and filer container */}
+      <div className='toolbar-actions-container'>
+        <SortButton
+          currentSort={currentSort}
+          onSortChange={onSortChange}
+        />
 
-        {/* Dropdown Sort */}
-        <div className='dropdown-sort'>
-          <button className='sort' onClick={() => setIsSortOpen(!isSortOpen)}>
-            <span id="current-sort">{currentSort}</span>
-            <img src={downArrowIcon} alt="arrow" />
-          </button>
-
-          {/* Logic to show/hide content */}
-          {isSortOpen && (
-            <div className='dropdown-content' id='sort-options'>
-              <button onClick={() => handleSortClick("Newest")}>Newest</button>
-              <button onClick={() => handleSortClick("Oldest")}>Oldest</button>
-              <button onClick={() => handleSortClick("Popular")}>Popular</button>
-            </div>
-          )}
-        </div>
-
-        {/* Dropdown Filter */}
-        <div className='dropdown-filter'>
-          <button className='filter' onClick={() => setIsFilterOpen(!isFilterOpen)}>
-            <img src={filterIcon} alt="filter" />
-            <span id='current-filter'>{currentFilter}</span>
-          </button>
-
-          {isFilterOpen && (
-            <div className='dropdown-content' id='filter-options'>
-              <button onClick={() => handleFilterClick("Show All")}>Show All</button>
-              <button onClick={() => handleFilterClick("Ratings > 1000")}>Ratings &gt; 1000</button>
-              <button onClick={() => handleFilterClick("Ratings < 1000")}>Ratings &lt; 1000</button>
-            </div>
-          )}
-        </div>
+        <FilterButton
+          currentFilter={currentFilter}
+          onFilterChange={onFilterChange}
+        />
       </div>
     </div>
   );
