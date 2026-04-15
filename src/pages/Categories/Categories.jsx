0cd05a5bd54650c8d9a1b0c21375c4e5a0b8fd9a
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-04-15 13:25:40 +0700

    add nothing

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,6 +1,6 @@
 import './Categories.css';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
