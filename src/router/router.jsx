8dc89768574da522fbbae378fa2c034111c5586e
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-04-16 17:23:37 +0700

    add route for homepage

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 
+import HomePage from '../pages/Homepage/Homepage';
 import UserPage from '../pages/User/User';
 import UserProfilePage from '../pages/User_Profile/UserProfile';
 import UserCoursesPage from '../pages/User_Courses/User_Courses';
@@ -12,7 +13,8 @@ import CourseInfoPage from '../pages/CourseInfo/CourseInfo';
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/user/1/profile" replace />, // Redirect home to profile with default userId
+    element: <HomePage />,
+    // element: <Navigate to="/user/1/profile" replace />,
   },
   {
     path: "/categories",
